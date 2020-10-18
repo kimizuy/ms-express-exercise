4cac8dedf802f2636aa7ed4697cdec02454db50f
kimizuy <kimizuyamasaki@gmail.com>
2020-10-18 23:39:26 +0900

    update

--- app.js
+++ app.js
@@ -2,24 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/products', (req, res) => {
-  const products = [
-    {
-      id: 1,
-      name: 'hammer'
-    },
-    {
-      id: 2,
-      name: 'screwdriver'
-    },
-    ,
-    {
-      id: 3,
-      name: 'wrench'
-    }
-  ]
+let bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
+let products = []
+
+app.get('/products', (req, res) => {
   res.json(products)
 })
+
+app.put('/products', function (req, res) {
+  // implement
+})
+
+app.delete('/products/:id', function (req, res) {
+  // implement
+})
+
+app.get('/products', (req, res) => {
+  // implement
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
