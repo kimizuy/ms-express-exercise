64b29322142a0529e6d3a7de5b7785a1e3e45194
kimizuy <kimizuyamasaki@gmail.com>
2020-10-19 23:50:30 +0900

    implement crud

--- app.js
+++ app.js
@@ -7,20 +7,32 @@ app.use(bodyParser.json())
 
 let products = []
 
-app.get('/products', (req, res) => {
-  res.json(products)
+app.post('/products', (req, res) => {
+  const newProduct = { ...req.body, id: products.length + 1 }
+  products = [...products, newProduct]
+  res.json(newProduct)
 })
 
 app.put('/products', function (req, res) {
-  // implement
+  let updatedProduct
+  products = products.map((p) => {
+    if (p.id === req.body.id) {
+      updatedProduct = { ...p, ...req.body }
+      return updatedProduct
+    }
+    return p
+  })
+  res.json(updatedProduct)
 })
 
 app.delete('/products/:id', function (req, res) {
-  // implement
+  const deletedProduct = products.find((p) => p.id === +req.params.id)
+  products = products.filter((p) => p.id !== +req.params.id)
+  res.json(deletedProduct)
 })
 
 app.get('/products', (req, res) => {
-  // implement
+  res.json(products)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
